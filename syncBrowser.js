e09f56d87e7df0ec642262a8b3d7e09ed33c14be
BenjaminCrozier <BenjaminCrozier+github@gmail.com>
2019-08-09 20:22:41 -0500

    init

---
+++ syncBrowser.js
@@ -0,0 +1,19 @@
+//SYNC 
+(function () {
+    console.log("syncBrowser");
+    var pageHash = 0;
+    var pingCount = 0;
+    function ping() {
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET', '?ping&' + pingCount++);
+        xhr.onload = function () {
+            if (!pageHash)
+                pageHash = xhr.response.length; //set length
+            if (pageHash != xhr.response.length)
+                location.reload(); //refresh updated page
+        };
+        xhr.send();
+    }
+    setInterval(ping, 3000); //3 seconding loop
+})();
+// /SYNC
